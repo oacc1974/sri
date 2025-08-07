d75029d29e2e66be403aa517a6ee93e3e021d816
oacc1974 <oscarcastrocantos@gmail.com>
2025-08-07 12:36:57 -0500

    Add files via upload

--- xml-signer.js
+++ xml-signer.js
@@ -466,12 +466,13 @@ async function signXml(xmlString, certificatePath, certificatePassword) {
         'http://www.w3.org/2000/09/xmldsig#enveloped-signature',
         'http://www.w3.org/TR/2001/REC-xml-c14n-20010315'
       ],
-      'http://www.w3.org/2000/09/xmldsig#sha1',
-      '',
-      '',
-      '',
-      true
+      'http://www.w3.org/2000/09/xmldsig#sha1'
     );
+    
+    // Configurar algoritmos de firma y digest
+    sig.signatureAlgorithm = "http://www.w3.org/2000/09/xmldsig#rsa-sha1";
+    sig.canonicalizationAlgorithm = "http://www.w3.org/TR/2001/REC-xml-c14n-20010315";
+    sig.digestAlgorithm = "http://www.w3.org/2000/09/xmldsig#sha1";
     
     // Configurar la clave de firma usando la clave privada en formato PEM
     try {
